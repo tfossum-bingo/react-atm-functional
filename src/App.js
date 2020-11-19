de69e7b6f3b403910cfcc084543f55a74ca7f693
Andre Pato <anpato1994@gmail.com>
2020-11-19 00:48:34 -0500

    fix start bug and restructure folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 // NPM modules
-import React from 'react';
+import React from 'react'
 
 // Custom Components
-import Account from './Account';
+import Account from './components/Account'
 
 // Resources
-import logo from './ga.png';
-import './App.css';
+import logo from './ga.png'
+import './styles/App.css'
 
 function App() {
   return (
     <div id="content">
       <div id="nav">
-        <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
+        <div id="logo">
+          <img src={logo} alt="General Assembly logo" />
+        </div>
         <div id="title">Bank of GA</div>
       </div>
       <Account name="Checking" />
       <div className="clear"></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
